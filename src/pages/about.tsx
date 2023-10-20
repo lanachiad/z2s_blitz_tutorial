26f61116e4581d4378664c28a949c9bd0bb5d185
Lana Chiad <lana@jumper.com>
2023-10-20 12:50:44 -0400

    cleanup blitz pages, extract components

---
+++ src/pages/about.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BlitzPage } from "@blitzjs/next";
+
+const AboutPage: BlitzPage = () => {
+  return <div>This is the about page.</div>;
+};
+
+export default AboutPage;
